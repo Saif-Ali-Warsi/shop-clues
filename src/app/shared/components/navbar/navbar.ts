c440af1e0cc6e7936ec1f796c99d0b7979f882b2
saif <warsivorlox@gmail.com>
2026-07-04 16:19:45 +0530

    categories added

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CartService } from '../../../features/cart/services/cart-service';
 import { AuthService } from '../../../features/auth/services/auth-service';
-
+import { ProductService } from '../../../features/products/services/product-service';
+import { Category } from '../../../features/products/models/category.model';
 
 @Component({
   selector: 'app-navbar',
@@ -14,15 +15,25 @@ export class Navbar implements OnInit {
 
   private cartService = inject(CartService);
   authService = inject(AuthService);
+  private productService = inject(ProductService)
 
   cartCount = this.cartService.cartCount;
+  categories: Category[] = [];
 
   ngOnInit(): void {
-
+    this.loadCategories()
   }
 
   logout() {
     this.authService.logout()
+  }
+
+  loadCategories() {
+    this.productService.getCategories().subscribe({
+      next: (categories) => {
+        this.categories = categories
+      }
+    })
   }
 
 
