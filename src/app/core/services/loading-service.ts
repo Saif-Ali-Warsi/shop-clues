77b4637ac17c8670a3ab5b245714f4e986a2527a
saif <warsivorlox@gmail.com>
2026-07-12 00:28:04 +0530

    loader replacement done

--- src/app/core/services/loading-service.ts
+++ src/app/core/services/loading-service.ts
@@ -5,14 +5,21 @@ import { Injectable, signal } from '@angular/core';
 })
 export class LoadingService {
 
-  isLoading = signal(false)
+  isLoading = signal(false);
 
   show() {
-    this.isLoading.set(true)
+    this.isLoading.set(true);
+    
+    if (this.isLoading()) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
   }
 
   hide() {
     this.isLoading.set(false)
+    document.body.classList.remove('no-scroll');
   }
 
 }
