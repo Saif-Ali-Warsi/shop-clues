fcebbf1c4acc7c4eecb97cbf57e9ea406df0cc1c
saif <warsivorlox@gmail.com>
2026-07-11 15:59:24 +0530

    category modifications done

--- src/app/features/products/components/category-card/category-card.ts
+++ src/app/features/products/components/category-card/category-card.ts
@@ -1,7 +1,12 @@
 import { Component, input } from '@angular/core';
-import { Category } from '../../models/category.model';
 import { RouterLink } from '@angular/router';
 
+
+interface Category {
+  title: string;
+  image: string;
+  url:string;
+}
 
 @Component({
   selector: 'app-category-card',
@@ -11,6 +16,67 @@ import { RouterLink } from '@angular/router';
 })
 export class CategoryCard {
 
-  category = input.required<Category>();
 
+  categories: Category[] = [
+    {
+      title: 'Mobile Phones',
+      image: 'https://superkart-demo.mybigcommerce.com/product_images/uploaded_images/category-1.png',
+      url:'products?categories=smartphones'
+    },
+    {
+      title: 'Laptops & Tablets',
+      image: 'https://superkart-demo.mybigcommerce.com/product_images/uploaded_images/category-2.png',
+      url:'products?categories=laptops'
+    },
+    {
+      title: 'Televisions',
+      image: 'https://superkart-demo.mybigcommerce.com/product_images/uploaded_images/category-3.png',
+      url:'products?categories=tablets'
+    },
+    {
+      title: 'PCs & Accessories',
+      image: 'https://superkart-demo.mybigcommerce.com/product_images/uploaded_images/category-4.png',
+      url:'products?categories=mobile-accessories'
+    },
+    {
+      title: 'Audio & Video',
+      image: 'https://superkart-demo.mybigcommerce.com/product_images/uploaded_images/category-5.png',
+      url:'products?categories=sports-accessories'
+    },
+    {
+      title: 'Gaming & Accessories',
+      image: 'https://superkart-demo.mybigcommerce.com/product_images/uploaded_images/category-6.png',
+      url:'products?categories=sunglasses'
+    },
+    {
+      title: 'Home Appliances',
+      image: 'https://superkart-demo.mybigcommerce.com/product_images/uploaded_images/category-7.png',
+      url:'products?categories=kitchen-accessories'
+    },
+    {
+      title: 'Kitchen Appliances',
+      image: 'https://superkart-demo.mybigcommerce.com/product_images/uploaded_images/category-8.png',
+      url:'products?categories=kitchen-accessories'
+    },
+    {
+      title: 'Cameras',
+      image: 'https://superkart-demo.mybigcommerce.com/product_images/uploaded_images/category-9.png',
+      url:'products?categories=mens-watches'
+    },
+    {
+      title: 'Consumables',
+      image: 'https://superkart-demo.mybigcommerce.com/product_images/uploaded_images/category-10.png',
+      url:'products?categories=groceries'
+    },
+    {
+      title: 'Smart Homes',
+      image: 'https://superkart-demo.mybigcommerce.com/product_images/uploaded_images/category-11.png',
+      url:'products?categories=home-decoration'
+    },
+    {
+      title: 'Accessories',
+      image: 'https://superkart-demo.mybigcommerce.com/product_images/uploaded_images/category-12.png',
+      url:'products?categories=mobile-accessories'
+    }
+  ];
 }
--- src/app/features/products/pages/home-page/home-page.ts
+++ src/app/features/products/pages/home-page/home-page.ts
@@ -1,6 +1,5 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product-service';
-import { Category } from '../../models/category.model';
 import { CategoryCard } from '../../components/category-card/category-card';
 import { Product } from '../../models/product.model';
 import { ProductCard } from '../../components/product-card/product-card';
@@ -20,13 +19,30 @@ export class HomePage implements OnInit {
 
   trendingProducts: Product[] = [];
   bestSeller: Product[] = [];
-  categories: Category[] = [];
+
 
 
   homeBanners = [
-    { img: "https://www.image2url.com/r2/default/images/1783437767428-c5690603-4599-4190-9471-c523d1494dcf.jpg" },
-    { img: "https://www.image2url.com/r2/default/images/1783437767428-c58fb5f8-5ada-4b4c-bc99-faa4355c6ad7.jpg" },
+    {
+      desktopImgUrl: [
+        {
+          img: 'https://www.image2url.com/r2/default/images/1783437767428-c5690603-4599-4190-9471-c523d1494dcf.jpg'
+        },
+        {
+          img: 'https://www.image2url.com/r2/default/images/1783437767428-c58fb5f8-5ada-4b4c-bc99-faa4355c6ad7.jpg'
+        }
+      ],
+      mobileImgUrl: [
+        {
+          img: 'https://cdn2.shopclues.com/images/banners/2026/april/07/rocket-deal-app-07April2026.jpg'
+        },
+        {
+          img: 'https://cdn2.shopclues.com/images/banners/2025/Oct/16/Intell-App-16Oct2025.jpg'
+        }
+      ]
+    }
   ];
+  
 
   quickCards = [
     { img: "https://www.image2url.com/r2/default/images/1783438427777-05fca0a0-c04a-4197-83c3-99800c8a908f.webp" },
@@ -39,15 +55,6 @@ export class HomePage implements OnInit {
   ngOnInit() {
     this.loadTrendingProducts();
     this.bestSellerProducts();
-    this.loadCategories();
-  }
-
-  loadCategories() {
-    this.productService.getCategories().subscribe({
-      next: (categories) => {
-        this.categories = categories
-      }
-    })
   }
 
   loadTrendingProducts() {
@@ -65,9 +72,6 @@ export class HomePage implements OnInit {
       }
     })
   }
-
-
-
 
 
 }
