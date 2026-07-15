fa12da0a946811ad99cdb77aa65ebc1091ddf935
saif <warsivorlox@gmail.com>
2026-07-15 21:13:39 +0530

    developer signature in footer added

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
+
+  currentYear = Date()
 
 }
